e21511518aae32cb0979abda13bcc8e6d5d3b6ec
Parapass <parapasser@gmail.com>
2023-05-04 17:35:57 -0400

    thug shaking by getting getUser() done

--- main.py
+++ main.py
@@ -29,41 +29,48 @@ def getUser():
     # find a way to make it so you dont have to type 1 bajillion gazillion numbers
     response = requests.get(f"{SCORESABER_API}/player/{steamID}/full").json()
     # most sane python programmer
-    print(f'Name - {response["name"]}')
-    print(f'Country - {response["country"]}')
-    print(f'Global Rank - #{response["rank"]}')
-    print(f'Country Rank - #{response["countryRank"]}')
-    print(f'PP (Performance Points) - {response["pp"]}')
-    print(f'Total Score - {response["scoreStats"]["totalScore"]}')
-    print(f'Total Ranked Score - {response["scoreStats"]["totalRankedScore"]}')
-    print(f'Total Play Count - {response["scoreStats"]["totalPlayCount"]}')
-    print(f'Ranked Play Count - {response["scoreStats"]["rankedPlayCount"]}')
-    # actually i probably could use \n to refactor but NAHHHHHH
+    if "errorMessage" in response:
+        print(f'Error: {response["errorMessage"]}. Please re-enter your SteamID.\n')
+        getUser()
+    else:
+        print(f'Name - {response["name"]}')
+        print(f'Country - {response["country"]}')
+        print(f'Global Rank - #{response["rank"]}')
+        print(f'Country Rank - #{response["countryRank"]}')
+        print(f'PP (Performance Points) - {response["pp"]}')
+        print(f'Total Score - {response["scoreStats"]["totalScore"]}')
+        print(f'Total Ranked Score - {response["scoreStats"]["totalRankedScore"]}')
+        print(f'Total Play Count - {response["scoreStats"]["totalPlayCount"]}')
+        print(f'Ranked Play Count - {response["scoreStats"]["rankedPlayCount"]}')
+    # most optimized python code to ever exist
     scoreConfirm = input("Would you like to see the top scores of this user? (y/n)\n>>> ")
     if scoreConfirm == "y":
-        # woopsie diddily doo!
-        print("Error: NotImplemented. Please try again later.\n")
-        # time to find out how to pull these scores
-        # watch it be something dumb like /player/scores
+        scoreResp = requests.get(f"{SCORESABER_API}/player/{steamID}/scores?limit=10&sort=top").json()
+        for score in scoreResp["playerScores"]:
+            print(f'\nSong Name - {score["leaderboard"]["songName"]}')
+            print(f'Song Author - {score["leaderboard"]["songAuthorName"]}')
+            print(f'Level Creator - {score["leaderboard"]["levelAuthorName"]}')
+            print(f'Rank - {score["score"]["rank"]}')
+            print(f'Score - {score["score"]["modifiedScore"]}')
+            # we want to use modified score here because what if their top play is overkill DAFS??? HUH????
+            print(f'PP (unranked will be 0) - {score["score"]["pp"]}')
+        # didnt work ima use chatgpt
+        # I GOT IT HIP HIP HOORAY!
         start()
     elif scoreConfirm == "n":
         print("Returning to main screen...\n")
         start()
-    elif scoreConfirm == "db":
-        # debug mode for advanced users (me)
-        print(f"Full API Response (db) - {response}\n")
-        input("Press Enter to continue... ")
-        start()
+        # rip debug mode we done did kil it :(
 
 def getMap():
     mapHash = input("Enter ScoreSaber map hash\n>>> ")
-    # we need input here but im lazy so i havent added anything here yet
+    # i will work on this later but it will never get done. Sorry!
     print("Error: NotImplemented. Please try again later.\n")
     start()
 
 def limitedTest():
     # just the same code from above but it doenst parse the json!!!
-    steamID = input("Please enter the SteamID of the user you are looking for (example: 76561198335894744)\n>>> ")
+    steamID = input("Please enter the SteamID of the user you are looking for (You are testing for score output)\n>>> ")
     response = requests.get(f'{SCORESABER_API}/player/{steamID}/full')
     response = response.text
     response = json.loads(response)
